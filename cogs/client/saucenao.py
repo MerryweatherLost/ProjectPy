fd25f9f0cf747984308ee20b150cf70529b17bf5
Raymond Allison <redwitblue@gmail.com>
2021-10-30 13:27:00 -0500

    New Saucenao Commands
    Pixnao - Returns Pixiv
    Boorunao - Returns Booru (weird state at the moment)
    Animenao - Returns Anime (need to set attributes)

---
+++ cogs/client/saucenao.py
@@ -0,0 +1,115 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+from pysaucenao.containers import TwitterSource
+from pysaucenao.containers import MangaSource
+
+from library.ConsoleSelect import Time;
+from library.ConsoleSelect import Roundtrip;
+
+from pysaucenao import SauceNao, PixivSource, BooruSource, AnimeSource
+
+class SauceNow(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # PIXNAO - METHOD
+    @commands.command(help = 'Delivers the sauce by image. [Pixiv]')
+    async def pixnao(self, ctx, image: str):
+        sauce = SauceNao(api_key = 'ad5dd123e3b1b2ccdd38575f2736287b0aea1e26')
+        results = await sauce.from_url(url = image); len(results)
+
+        if isinstance(results[0], PixivSource):
+            message: discord.Message = await ctx.reply('I found a match... <a:checkmark:903852585360949289> gimmie me a second... <a:cleo:902025889179631637>')
+
+            embed = discord.Embed (
+                title = f'{results[0].title}',
+                url = f'{results[0].url}'
+            )
+            embed.set_thumbnail (
+                url = f'{results[0].thumbnail}'
+            )
+            embed.add_field (name = 'Type', value = f'{results[0].type.capitalize()}')
+            embed.add_field (name = 'Similarity', value = f'{results[0].similarity}%')
+            embed.add_field (name = 'Author Name', value = f'{results[0].author_name}')
+            embed.add_field (name = 'Author Link', value = f'[Link]({results[0].author_url})')
+            embed.add_field (name = 'Index ID', value = f'{results[0].index_id}')
+
+            await message.delete()
+            await ctx.reply(embed = embed)
+            print(f'[{Time.timeCST()}] [Roundtrip: {Roundtrip.rt(self)}ms.] CONSOLE: SAUCENAO.PY - LOG: SauceNao [Pixiv] was utilized in #{ctx.channel}! \n[Raw Data: {results[0].url}]')
+            print(f'[Short Remainder: {results.short_remaining}] [Long Remainder: {results.long_remaining}]')
+        else:
+            embed = discord.Embed(description = 'I can not find this within the Pixiv Category, try another one or get a clearer image.')
+            await ctx.reply(embed = embed)
+    
+    # BOORUNAO - METHOD
+    @commands.command(help = 'Delivers the sauce by image. [Gelbooru/Danbooru]')
+    async def boorunao(self, ctx, image: str):
+        sauce = SauceNao(api_key = 'ad5dd123e3b1b2ccdd38575f2736287b0aea1e26')
+        results = await sauce.from_url(url = image); len(results)
+
+        if isinstance(results[0], BooruSource):
+            message: discord.Message = await ctx.reply('I found a match... <a:checkmark:903852585360949289> gimmie me a second... <a:cleo:902025889179631637>')
+
+            embed = discord.Embed (
+                title = f'{results[0].title}',
+                url = f'{results[0].url}'
+            )
+            embed.set_thumbnail (
+                url = f'{results[0].thumbnail}'
+            )
+            embed.add_field (name = 'Type', value = f'{results[0].type.capitalize()}')
+            embed.add_field (name = 'Similarity', value = f'{results[0].similarity}%')
+            embed.add_field (name = 'Author Name', value = f'{results[0].author_name}')
+            embed.add_field (name = 'Author Link', value = f'[Link]({results[0].author_url})')
+            embed.add_field (name = 'Index ID', value = f'{results[0].index_id}')
+
+            embed.set_footer (
+                text = f'Source URL: {results[0].source_url} Index: {results[0].index}'
+            )
+            await message.delete()
+            await ctx.reply(embed = embed)
+            print(f'[{Time.timeCST()}] [Roundtrip: {Roundtrip.rt(self)}ms.] CONSOLE: SAUCENAO.PY - LOG: SauceNao [Gelbooru/Danbooru] was utilized in #{ctx.channel}!')
+            print(f'[Raw Data: {results[0].url}] [Short Remainder: {results.short_remaining}] [Long Remainder: {results.long_remaining}]')
+        else:
+            embed = discord.Embed(description = 'I can not find this within the Booru Category, try another one or get a clearer image.')
+            await ctx.reply(embed = embed)
+
+    # ANIMENAO - METHOD
+    @commands.command(help = 'Delivers the sauce by image. [Anime, Eroge, Etc...]')
+    async def animenao(self, ctx, image: str):
+        sauce = SauceNao(api_key = 'ad5dd123e3b1b2ccdd38575f2736287b0aea1e26')
+        results = await sauce.from_url(url = image); len(results)
+
+        if isinstance(results[0], AnimeSource):
+            message: discord.Message = await ctx.reply('I found a match... <a:checkmark:903852585360949289> gimmie me a second... <a:cleo:902025889179631637>')
+            
+            embed = discord.Embed (
+                title = f'{results[0].title}',
+                url = f'{results[0].url}'
+            )
+            embed.set_thumbnail (
+                url = f'{results[0].thumbnail}'
+            )
+            embed.add_field (name = 'Type', value = f'{results[0].type.capitalize()}')
+            embed.add_field (name = 'Similarity', value = f'{results[0].similarity}%')
+            embed.add_field (name = 'Author Name', value = f'{results[0].author_name}')
+            embed.add_field (name = 'Author Link', value = f'[Link]({results[0].author_url})')
+            embed.add_field (name = 'Index ID', value = f'{results[0].index_id}')
+
+            embed.set_footer (
+                text = f'Source URL: {results[0].source_url} Index: {results[0].index}'
+            )
+            await message.delete()
+            await ctx.reply(embed = embed)
+            print(f'[{Time.timeCST()}] [Roundtrip: {Roundtrip.rt(self)}ms.] CONSOLE: SAUCENAO.PY - LOG: SauceNao [Anime] was utilized in #{ctx.channel}! \n[Raw Data: {results[0].url}]')
+            print(f'[Short Remainder: {results.short_remaining}] [Long Remainder: {results.long_remaining}]')
+        else:
+            embed = discord.Embed(description = 'I can not find this within the Anime Category, try another one or get a clearer image.')
+            await ctx.reply(embed = embed)
+    
+
+def setup(client):
+    client.add_cog(SauceNow(client))
