68420f699cfdd6964343ce71b59188600c744718
Raymond Allison <redwitblue@gmail.com>
2021-11-04 23:34:29 -0500

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -15,6 +15,10 @@ class General:
     def quote():
         quotelist = [
             u'\u001b[34;1mHey, who turned the lights off?\u001b[0m',
+            u'\u001b[34;1mObject-Oriented Programming is still relevant.\u001b[0m',
+            u'\u001b[34;1mWho even writes this stuff anyway?\u001b[0m',
+            u'\u001b[34;1mHey, who turned the lights off?\u001b[0m',
+            u'\u001b[34;1mFortran is the best programming language.\u001b[0m',
             u'\u001b[34;1mAmerica grew up listening to us. It still does.\u001b[0m',
         ]
         quote = random.choice(quotelist)
